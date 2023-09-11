18856ea5d7d1e90d590edb413c533c129163cc99
TaliaBarbosa <talialadislau.13@gmail.com>
2023-09-11 09:33:52 -0300

    carregando

--- js/paginas.js
+++ js/paginas.js
@@ -6,7 +6,19 @@ class paginas{
     PaginaDeProduto(){
         this.PaginaPrincipal.innerHTML = "ola mundo"
         this.InserirConteudo("<div id='VitrineProdutos'></div>")
-        this.cards.CriarCards("VitrineProdutos","Tenis da Nike","Produtos novo da nike")
+        this.cards.CriarCards("VitrineProdutos","Tenis da Nike","Produtos novo da loja")
+        document.getElementsByClassName("card")[0].id = "Nike_prod"
+        let Produto_1 = document.getElementById("Nike_prod")
+        Produto_1.addEventListener("click",this.PaginaDeProduto_Adidas)
+
+
+    }
+    PaginaDeProduto_Adidas(){
+        //this.PaginaPrincipal.innerHTML = ""
+        //this.InserirConteudo("<div class='produtos'> Tenis da Adidas </div>")
+        document.getElementsByClassName("localNav")[0].innerHTML = "<h4>Produtos/ Tenis da Adidas </h4>"
+        document.getElementById("pagina").innerHTML = ""
+        document.getElementById("pagina").innerHTML+="<div class='produtos'></div>"
     }
     InserirConteudo(conteudo){
         this.PaginaPrincipal.innerHTML+=conteudo
